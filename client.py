439559d1cea52e7a2dbdfe913242641ac16e11b1
Jacob Rodgers <yudjinncoding@gmail.com>
2020-07-10 22:33:18 -0600

    Simple socket program pinging from client to server

---
+++ client.py
@@ -0,0 +1,23 @@
+import socket
+
+HEADER = 128
+PORT = 5050
+FORMAT = 'utf-8'
+DISCONNECT_MSG = "!DISCONNECT"
+SERVER = "127.0.1.1"
+ADDR = (SERVER, PORT)
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_len = len(message)
+    send_len = str(msg_len).encode(FORMAT)
+    send_len += b' ' * (HEADER - len(send_len))
+    client.send(send_len)
+    client.send(message)
+    print(client.recv(2048).decode(FORMAT))
+
+send("Hello world")
+send(DISCONNECT_MSG)
